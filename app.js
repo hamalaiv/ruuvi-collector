68fda823396343697ccf7795bc154aef93744946
hamalaiv <hamalainen.ville.p@gmail.com>
2019-07-07 18:28:04 +0300

    write data to csv file

--- app.js
+++ app.js
@@ -5,10 +5,22 @@ const ruuvi = os.type() == "Windows_NT" ?
                 require("./ruuvi-simulator.js") :
                 require("node-ruuvitag");
 
+const rcfs = require("./rc-fs.js");
+
 ruuvi.on('found', tag => {
-  console.log('Found RuuviTag, id: ' + tag.id);
-  tag.on('updated', data => {
-    console.log('Got data from RuuviTag ' + tag.id + ':\n' +
-      JSON.stringify(data, null, '\t'));
+    console.log('Found RuuviTag, id: ' + tag.id);
+    tag.on('updated', data => {
+        let now = new Date().toISOString();
+
+        // add more fields to data object
+        data.id = tag.id;
+        data.timestamp = now;
+        
+        // log to console
+        // console.log(JSON.stringify(data, null, '\t'));
+        console.log(JSON.stringify(data, null, " "));
+        
+        // store to file
+        rcfs.store(data, now);
   });
 });
---
+++ rc-fs.js
@@ -0,0 +1,54 @@
+/*
+ * csv file headers:
+ * id;timestamp;rssi;humidity;temperature;pressure;accelerationX;accelerationY;accelerationZ;battery
+ * 
+*/
+
+const fs = require("fs");
+const fsPromises = fs.promises;
+var path = require('path');
+
+class RCFS{
+    
+    constructor(){
+        this.baseFolder = path.join(__dirname, "data");
+    }
+
+    async store(data){
+        let folderExists = await this._ensureFolderExists();
+        if(!folderExists){
+            return false;
+        }
+
+        return this._writeToFile(data);
+    }
+
+    async _ensureFolderExists(){
+        if(!fs.existsSync(this.baseFolder)){
+            try{
+                await fsPromises.mkdir(this.baseFolder);
+            }
+            catch(error){
+                console.error(`Error RCFS._ensureFolderExists: ${error}`);
+                return false;
+            }
+        }
+        return true;
+    }
+
+    async _writeToFile(data){
+        let filePath = path.join(this.baseFolder, `${data.id}.csv`);
+        let entry = `${data.id};${data.timestamp};${data.rssi};${data.humidity};${data.temperature};${data.pressure};${data.accelerationX};${data.accelerationY};${data.accelerationZ};${data.battery};\n`;
+        try{
+            await fsPromises.appendFile(filePath, entry);
+        }
+        catch(error){
+            console.error(`Error RCFS._writeToFile: ${error}`);
+            return false;
+        }
+
+        return true;
+    }
+}
+
+const rcfs = module.exports = new RCFS();
