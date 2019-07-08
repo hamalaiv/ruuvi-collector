5730802e4dfdcfac463375a40433de6bac8d4b4f
hamalaiv <hamalainen.ville.p@gmail.com>
2019-07-08 09:35:49 +0300

    Use python script to run nodejs app
    - Change nodejs app to get only one measurement from ruuvi-tag and then exit
    - Run the nodejs app with python app in a loop and sleep in between

--- app.js
+++ app.js
@@ -1,25 +1,43 @@
 const os = require("os");
-
-// simulate ruuvi tags on windows machines
-const ruuvi = os.type() == "Windows_NT" ? 
-                require("./ruuvi-simulator.js") :
-                require("node-ruuvitag");
-
 const rcfs = require("./rc-fs.js");
 
+const ruuvi = os.type() == "Windows_NT" ?
+                            require("./ruuvi-simulator.js") :
+                            require("node-ruuvitag");
+
 ruuvi.on('found', tag => {
-    console.log('Found RuuviTag, id: ' + tag.id);
-    tag.on('updated', data => {
+    console.log(`found [${tag.id}]`);
+    tag.on('updated', async data => {
         let now = new Date().toISOString();
 
         // add more fields to data object
         data.id = tag.id;
         data.timestamp = now;
-        
+
         // log to console
-        console.log(JSON.stringify(data, null, '\t'));
-        
+        console.log(`updated [${tag.id}] rssi=${data.rssi}, humd=${data.humidity.toFixed(2)}, temp=${data.temperature.toFixed(2)}, pres=${data.pressure}, accX=${data.accelerationX}, accY=${data.accelerationY}, accZ=${data.accelerationZ}, batt=${data.battery}`);
+
         // store to file
-        rcfs.store(data, now);
-  });
+        await rcfs.store(data, now);
+
+        // data processed, can exit now succesfully
+        process.exit();
+    });
 });
+
+async function start() {
+    // wait for ruuvi-tag to be found
+    await sleep(10000);
+
+    // did not find ruuvi-tag,
+    // exit with failed status
+    process.exit(1);
+}
+
+function sleep(ms) {
+    return new Promise(resolve => {
+        setTimeout(resolve, ms)
+    })
+}
+
+start();
---
+++ app.py
@@ -0,0 +1,7 @@
+import subprocess
+import time
+
+while True:
+    result = subprocess.check_call('node app.js', shell=True)
+    print("%s result: %s" % (time.strftime("%d.%m %H:%M:%S"), result))
+    time.sleep(10)
