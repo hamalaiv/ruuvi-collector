998f7ea5d16af9fed17cd818e54db3605fd7ade6
hamalaiv <hamalainen.ville.p@gmail.com>
2019-07-08 10:40:50 +0300

    Refactor rc-fs to not use fsPromises
    The feature is too new, cannot use it at least yet because it does not work with nodejs 8.x

--- rc-fs.js
+++ rc-fs.js
@@ -5,7 +5,6 @@
 */
 
 const fs = require("fs");
-const fsPromises = fs.promises;
 var path = require('path');
 const log = require("./log.js");
 
@@ -15,40 +14,50 @@ class RCFS{
         this.baseFolder = path.join(__dirname, "data");
     }
 
-    async store(data){
-        let folderExists = await this._ensureFolderExists();
-        if(!folderExists){
-            return false;
-        }
+    store(data){
+        return new Promise(async resolve => {
+            let folderExists = await this._ensureFolderExists();
+            if(!folderExists){
+                resolve(false);
+                return;
+            }
 
-        return this._writeToFile(data);
+            let writeSucceeded = await this._writeToFile(data);
+            resolve(writeSucceeded);
+        });
     }
 
-    async _ensureFolderExists(){
-        if(!fs.existsSync(this.baseFolder)){
-            try{
-                await fsPromises.mkdir(this.baseFolder);
+    _ensureFolderExists(){
+        return new Promise(resolve => {
+            if(fs.existsSync(this.baseFolder)){
+                resolve(true);
+                return;
             }
-            catch(error){
-                log.e(`Error RCFS._ensureFolderExists: ${error}`);
-                return false;
-            }
-        }
-        return true;
+
+            fs.mkdir(this.baseFolder, error => {
+                if(error){
+                    log.e(`Error RCFS._ensureFolderExists: ${error}`);
+                    resolve(false);
+                    return;
+                }
+                resolve(true);
+            });
+        });
     }
 
-    async _writeToFile(data){
-        let filePath = path.join(this.baseFolder, `${data.id}.csv`);
-        let entry = `${data.id};${data.timestamp};${data.rssi};${data.humidity};${data.temperature};${data.pressure};${data.accelerationX};${data.accelerationY};${data.accelerationZ};${data.battery};\n`;
-        try{
-            await fsPromises.appendFile(filePath, entry);
-        }
-        catch(error){
-            log.e(`Error RCFS._writeToFile: ${error}`);
-            return false;
-        }
-
-        return true;
+    _writeToFile(data){
+        return new Promise(resolve => {
+            let filePath = path.join(this.baseFolder, `${data.id}.csv`);
+            let entry = `${data.id};${data.timestamp};${data.rssi};${data.humidity};${data.temperature};${data.pressure};${data.accelerationX};${data.accelerationY};${data.accelerationZ};${data.battery};\n`;
+            fs.appendFile(filePath, entry, error => {
+                if(error){
+                    log.e(`Error RCFS._writeToFile: ${error}`);
+                    resolve(false);
+                    return;
+                }
+                resolve(true);
+            });
+        });
     }
 }
 
