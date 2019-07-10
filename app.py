e79d6e422ee74bd5f13969ff1bbb43a12f250bd6
hamalaiv <hamalainen.ville.p@gmail.com>
2019-07-10 10:23:12 +0300

    Change data retrieval interval to 15min

--- app.py
+++ app.py
@@ -4,4 +4,4 @@ import time
 while True:
     result = subprocess.check_call('node app.js', shell=True)
     print("%s result: %s" % (time.strftime("%d.%m %H:%M:%S"), result))
-    time.sleep(60)
+    time.sleep(900)
